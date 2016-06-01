20ca18bdca6bdcfe49014dca9873a61cc7280446
larmas <lucas.armas.7@gmail.com>
2016-06-01 00:10:07 -0300

    Upload scripts to create the tables in the database and load data into the same

---
+++ Code/createTables.sql
@@ -0,0 +1,146 @@
+﻿DROP SCHEMA IF EXISTS CiudadDeLosNiños CASCADE;
+CREATE SCHEMA CiudadDeLosNiños;
+SET search_path to CiudadDeLosNiños;
+
+CREATE TABLE persona (
+dni INTEGER NOT NULL,
+nombre VARCHAR(40) NOT NULL,
+apellido VARCHAR(40) NOT NULL,
+direccion VARCHAR(60) NOT NULL,
+cod_postal INTEGER NOT NULL,
+e_mail VARCHAR(40),
+facebook VARCHAR(40),
+fecha_nac DATE NOT NULL,
+edad INTEGER NOT NULL,
+tel_fijo INTEGER NOT NULL,
+tel_cel INTEGER,
+CONSTRAINT Persona_pkey PRIMARY KEY (dni),
+CONSTRAINT dni_valido CHECK (dni > 0),
+CONSTRAINT edad_valida CHECK (edad > 0)
+);
+
+CREATE TABLE padrino (
+dni INTEGER NOT NULL,
+CONSTRAINT pk_padrino PRIMARY KEY (dni),
+CONSTRAINT fk_padrino FOREIGN KEY (dni) REFERENCES persona ON DELETE CASCADE
+);
+
+CREATE TABLE donante (
+dni INTEGER NOT NULL,
+ocupacion VARCHAR NOT NULL,
+cuil_cuit INTEGER NOT NULL,
+CONSTRAINT pk_donante PRIMARY KEY (dni),
+CONSTRAINT fk_donante FOREIGN KEY (dni) REFERENCES padrino ON DELETE CASCADE
+);
+
+CREATE TABLE referente (
+dni INTEGER NOT NULL,
+CONSTRAINT pk_referente PRIMARY KEY (dni),
+CONSTRAINT fk_referente FOREIGN KEY (dni) REFERENCES persona ON DELETE CASCADE
+);
+
+CREATE DOMAIN ESTADOVALIDO AS VARCHAR(20)
+CHECK( VALUE IN ('Sin llamar','Error','En Gestion','Adherido','Amigo','No acepta','Voluntario'))
+NOT NULL;
+
+
+CREATE TABLE contacto (
+dni INTEGER NOT NULL,
+estado ESTADOVALIDO,
+fecha_alta DATE NOT NULL,
+fecha_baja DATE,
+fecha_rechazo_adhesion DATE,
+fecha_primer_contacto DATE NOT NULL,
+dni_referente INTEGER,
+comentario VARCHAR,
+relacion VARCHAR,
+CONSTRAINT fecha_alta_valida CHECK (fecha_primer_contacto <= fecha_alta),
+CONSTRAINT fecha_baja_valida CHECK (fecha_alta < fecha_baja),
+CONSTRAINT fecha_rechazo_valida CHECK (fecha_baja < fecha_rechazo_adhesion),
+CONSTRAINT pk_contacto PRIMARY KEY (dni),
+CONSTRAINT fk_contacto FOREIGN KEY (dni) REFERENCES padrino ON DELETE CASCADE,
+CONSTRAINT fk_referente FOREIGN KEY (dni_referente) REFERENCES referente ON DELETE CASCADE
+);
+
+CREATE TABLE programa (
+ id_programa INTEGER NOT NULL,
+ nombre VARCHAR NOT NULL,
+ descripcion VARCHAR,
+ CONSTRAINT programa_pkey PRIMARY KEY (id_programa),
+ CONSTRAINT programa_valido CHECK (id_programa > 0)
+ );
+ 
+CREATE TABLE medio_de_pago (
+cod_pago INTEGER NOT NULL,
+nombre_titular VARCHAR NOT NULL,
+CONSTRAINT medio_pago_pkey PRIMARY KEY (cod_pago),
+CONSTRAINT medio_pago_valido CHECK (cod_pago > 0)
+);
+
+ CREATE TABLE aporta (
+ dni_donante INTEGER NOT NULL,
+ id_programa INTEGER NOT NULL,
+ cod_pago INTEGER NOT NULL,
+ monto INTEGER NOT NULL,
+ frecuencia VARCHAR NOT NULL,
+ CONSTRAINT pk_aporta PRIMARY KEY (dni_donante, id_programa, cod_pago),
+ CONSTRAINT fk_donante_aporta FOREIGN KEY (dni_donante) REFERENCES donante ON DELETE CASCADE,
+ CONSTRAINT fk_programa_aporta FOREIGN KEY (id_programa) REFERENCES programa ON DELETE CASCADE,
+ CONSTRAINT fk_pago_aporta FOREIGN KEY (cod_pago) REFERENCES medio_de_pago ON DELETE CASCADE,
+ CONSTRAINT monto_positivo CHECK (monto > 0),
+ CONSTRAINT pago_Aporta_valido CHECK (cod_pago > 0)
+ );
+
+ CREATE TABLE nombre_empresa (
+ nombre_tarjeta VARCHAR (40) NOT NULL PRIMARY KEY
+ );
+
+ CREATE TABLE tarjeta (
+ cod_pago INTEGER NOT NULL,
+ cod_verificacion INTEGER NOT NULL,
+ vencimiento DATE NOT NULL,
+ numero_tarjeta INTEGER NOT NULL,
+ nombre_tarjeta VARCHAR (40) NOT NULL,
+ CONSTRAINT pk_cod_pago_tarjeta PRIMARY KEY (cod_pago),
+ CONSTRAINT fk_medio_pago_tarjeta FOREIGN KEY (cod_pago) REFERENCES medio_de_pago ON DELETE CASCADE,
+ CONSTRAINT fk_empresa_nom_tarjeta FOREIGN KEY (nombre_tarjeta) REFERENCES nombre_empresa ON DELETE CASCADE,
+ CONSTRAINT cod_pago_tarjeta_valido CHECK (cod_pago > 0)
+ );
+ 
+ CREATE TABLE debito_transferencia (
+ cod_pago INTEGER NOT NULL,
+ cbu INTEGER NOT NULL,
+ nro_cuenta INTEGER NOT NULL,
+ tipo VARCHAR NOT NULL,
+ sucursal_banco VARCHAR NOT NULL,
+ nombre_banco VARCHAR NOT NULL,
+ CONSTRAINT debito_cod_pago PRIMARY KEY (cod_pago),
+ CONSTRAINT debito_trans_cod_pago FOREIGN KEY (cod_pago) REFERENCES medio_de_pago ON DELETE CASCADE
+ );
+
+ CREATE TABLE auditoria (
+ 	dni INTEGER NOT NULL,
+ 	--nombre VARCHAR(40) NOT NULL,
+ 	--apellido VARCHAR(40) NOT NULL,
+ 	cuil_cuit INTEGER NOT NULL,
+ 	--direccion VARCHAR(60) NOT NULL,
+ 	--tel_fijo INTEGER NOT NULL,
+ 	usuario VARCHAR(60) NOT NULL,
+ 	fecha_eliminacion DATE NOT NULL,
+ 	CONSTRAINT pk_donante_auditoria PRIMARY KEY (dni),
+	CONSTRAINT fk_donante_auditoria FOREIGN KEY (dni) REFERENCES padrino ON DELETE CASCADE
+ 	);
+
+ CREATE OR REPLACE FUNCTION insertar_trigger() RETURNS TRIGGER AS $insertar$
+ DECLARE BEGIN
+ 		INSERT INTO Auditoria 
+ 		(dni, cuil_cuit,  usuario, fecha_eliminacion)
+ 		VALUES (OLD.dni, OLD.cuil_cuit, session_user, now());
+ 		RETURN NULL;
+ END;
+ $insertar$ LANGUAGE plpgsql;
+
+ CREATE TRIGGER insertar_auditoria AFTER DELETE
+ ON Donante FOR EACH ROW
+ EXECUTE PROCEDURE insertar_trigger();
+ 
---
+++ Code/setTables.sql
@@ -0,0 +1,191 @@
+﻿SET search_path to CiudadDeLosNiños;
+
+-- Carga de datos a la tabla persona
+INSERT INTO persona 
+(dni, nombre, apellido, fecha_nac, direccion, cod_postal, e_mail, facebook, edad, tel_fijo, tel_cel)
+VALUES (37108050, 'Matias', 'Pollo', '1995/04/27', 'Santa Rosa 442', 5427, 'matias_lvcb@hotmail.com', 'matias pollo', 21, 4914144, 4119989);
+
+INSERT INTO persona 
+(dni, nombre, apellido, fecha_nac, direccion, cod_postal, e_mail, facebook, edad, tel_fijo, tel_cel)
+VALUES (37108054, 'Matias', 'Fernandez', '1994/12/30', 'Pueyrredon 442', 5800, 'matias_fernandez@hotmail.com', 'matias fernandez', 18, 4914130, 4240323);
+
+INSERT INTO persona 
+(dni, nombre, apellido, fecha_nac, direccion, cod_postal, e_mail, facebook, edad, tel_fijo, tel_cel)
+VALUES (37108055, 'Maria', 'Dolores', '2000/04/20', 'Santa Rosa 1000', 5800, 'mariadolores@hotmail.com', 'maria dolores', 34, 4914399, 2233444);
+
+INSERT INTO persona 
+(dni, nombre, apellido, fecha_nac, direccion, cod_postal, e_mail, facebook, edad, tel_fijo, tel_cel)
+VALUES (37108056, 'Esperanza', 'Carlini', '1995/06/11', 'Alvear 300', 1000, null, null, 20, 4136689, null);
+
+INSERT INTO persona 
+(dni, nombre, apellido, fecha_nac, direccion, cod_postal, e_mail, facebook, edad, tel_fijo, tel_cel)
+VALUES (37108057, 'Lucas', 'Pomilio', '1985/02/05', 'Lugones 322', 5427, 'lucas_pomilio@hotmail.com', 'lucas pomilio', 45, 4914123, 4449979);
+
+INSERT INTO persona 
+(dni, nombre, apellido, fecha_nac, direccion, cod_postal, e_mail, facebook, edad, tel_fijo, tel_cel)
+VALUES (37108052, 'Gabriela', 'Gomez', '1978/03/19', 'Alvear 1234', 1000, 'gabi_gomez@hotmail.com', 'gabriela gomez', 15, 4914700, 4563412);
+
+-- Carga de datos a la tabla padrino
+INSERT INTO padrino 
+(dni)
+VALUES (37108050);
+
+INSERT INTO padrino 
+(dni)
+VALUES (37108054);
+
+INSERT INTO padrino 
+(dni)
+VALUES (37108055);
+
+INSERT INTO padrino 
+(dni)
+VALUES (37108056);
+
+--Carga de datos a la tabla referente
+
+INSERT INTO referente 
+(dni)
+VALUES (37108057);
+
+INSERT INTO referente 
+(dni)
+VALUES (37108052);
+
+-- Carga de datos a la tabla contacto
+
+INSERT INTO contacto 
+(dni, estado, fecha_primer_contacto, fecha_alta, fecha_baja, fecha_rechazo_adhesion, dni_referente, relacion, comentario)
+VALUES (37108050, 'Adherido', '2000/03/02', '2001/01/12', null, null, 37108057, 'Primos', 'Exelente persona');
+
+INSERT INTO contacto 
+(dni, estado, fecha_primer_contacto, fecha_alta, fecha_baja, fecha_rechazo_adhesion, dni_referente, relacion, comentario)
+VALUES (37108054, 'Adherido', '2012/03/02', '2012/04/12', null, null, 37108052, 'Conocidos', 'No paga a tiempo');
+
+INSERT INTO contacto 
+(dni, estado, fecha_primer_contacto, fecha_alta, fecha_baja, fecha_rechazo_adhesion, dni_referente, relacion, comentario)
+VALUES (37108055, 'Adherido', '1999/12/12', '2000/01/01', '2003/05/05', '2003/06/05', 37108057, 'Compañero de trabajo', 'Colaborador');
+
+INSERT INTO contacto 
+(dni, estado, fecha_primer_contacto, fecha_alta, fecha_baja, fecha_rechazo_adhesion, dni_referente, relacion, comentario)
+VALUES (37108056, 'Adherido', '2002/11/02', '20013/01/01', null, null, 37108052, 'Amigos', 'Cumplidor');
+
+-- Carga de datos a la tabla donantes
+
+INSERT INTO donante 
+(dni, ocupacion, cuil_cuit)
+VALUES (37108050, 'Empleado Rural', 123456789); 
+
+INSERT INTO donante 
+(dni, ocupacion, cuil_cuit)
+VALUES (37108054, 'Empresario', 987654321);
+
+INSERT INTO donante 
+(dni, ocupacion, cuil_cuit)
+VALUES (37108056, 'Ama de casa', 564738291);
+
+-- Carga de datos a la tabla programa
+
+INSERT INTO programa 
+(id_programa,nombre,descripcion)
+VALUES (1, 'Huerta', 'materiales para huerta');
+
+INSERT INTO programa 
+(id_programa,nombre,descripcion)
+VALUES (2, 'Carpinteria', 'herramientas');
+
+INSERT INTO programa 
+(id_programa,nombre,descripcion)
+VALUES (3, 'Apoyo', 'salario docente');
+
+INSERT INTO programa 
+(id_programa,nombre,descripcion)
+VALUES (4, 'Educacion fisica', 'materiales para educacion fisica');
+
+INSERT INTO programa 
+(id_programa,nombre,descripcion)
+VALUES (5, 'Comida', 'mercaderia');
+
+INSERT INTO programa 
+(id_programa,nombre,descripcion)
+VALUES (6, 'Taller', 'materiales para taller');
+
+-- Carga de datos a la tabla medio de pago
+
+INSERT INTO medio_de_pago 
+(cod_pago, nombre_titular)
+VALUES (001, 'Matias Pollo');
+
+INSERT INTO medio_de_pago 
+(cod_pago, nombre_titular)
+VALUES (123, 'Matias Fernandez');
+
+INSERT INTO medio_de_pago 
+(cod_pago, nombre_titular)
+VALUES (321, 'Maria Dolores');
+
+INSERT INTO medio_de_pago 
+(cod_pago, nombre_titular)
+VALUES (111, 'Esperanza Carlini');
+
+-- Carga de datos a la tabla aporta
+
+INSERT INTO aporta 
+(dni_donante, id_programa, cod_pago, monto, frecuencia)
+VALUES (37108050, 1, 001, 500, 'mensual');
+
+INSERT INTO aporta 
+(dni_donante, id_programa, cod_pago, monto, frecuencia)
+VALUES (37108054, 2, 123, 1000, 'mensual');
+
+INSERT INTO aporta 
+(dni_donante, id_programa, cod_pago, monto, frecuencia)
+VALUES (37108054, 4, 123, 300, 'mensual');
+
+INSERT INTO aporta 
+(dni_donante, id_programa, cod_pago, monto, frecuencia)
+VALUES (37108050, 3, 001, 1500, 'trimestral');
+
+INSERT INTO aporta 
+(dni_donante, id_programa, cod_pago, monto, frecuencia)
+VALUES (37108056, 5, 111, 4000, 'anual');
+
+-- Carga de datos a la tabla nombre empresa
+
+INSERT INTO nombre_empresa 
+(nombre_tarjeta)
+VALUES ('Naranja');
+
+INSERT INTO nombre_empresa 
+(nombre_tarjeta)
+VALUES ('BBVA Frances');
+
+INSERT INTO nombre_empresa 
+(nombre_tarjeta)
+VALUES ('Cordoba');
+
+INSERT INTO nombre_empresa 
+(nombre_tarjeta)
+VALUES ('Galicia');
+
+-- Carga de datos a la tabla tarjeta
+
+INSERT INTO tarjeta 
+(cod_pago, cod_verificacion, numero_tarjeta, vencimiento, nombre_tarjeta)
+VALUES (001, 987, 123456, '2020/10/14', 'Naranja');
+
+INSERT INTO tarjeta 
+(cod_pago, cod_verificacion, numero_tarjeta, vencimiento, nombre_tarjeta)
+VALUES (123, 789, 678901, '2022/12/01', 'BBVA Frances');
+
+-- Carga de datos a la tabla debito transferencia
+
+INSERT INTO debito_transferencia 
+( cod_pago, cbu, nro_cuenta, tipo, sucursal_banco, nombre_banco)
+VALUES (321, 5555, 0022, 'Tansferencia', 'Rio Cuarto', 'Galicia');
+
+INSERT INTO debito_transferencia 
+( cod_pago, cbu, nro_cuenta, tipo, sucursal_banco, nombre_banco)
+VALUES (111, 2222, 3344, 'Debito', 'Cordoba', 'Cordoba');
+
+
